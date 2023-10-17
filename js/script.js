3c97fd98483ff720d9760cf1e511b556c98d7bcc
Ahmad Sadati <a.sadati336@gmail.com>
2023-10-17 14:01:02 +0330

    added style changing js codes

--- js/script.js
+++ js/script.js
@@ -35,23 +35,52 @@ const lastname = document.getElementById("lastname");
 const signIn = document.getElementById("signIn");
 const output = document.getElementById("output");
 const code = document.getElementById("code");
-const fName = document.getElementById('f-Name')
-const nameList = []
-let nameListText = ""
+const fName = document.getElementById("f-Name");
+const nameList = [];
+let nameListText = "";
 signIn.addEventListener("click", () => {
-    if(!!name.value && !!lastname.value &&  !!code.value &&  !!fName.value ){
-        const reg = {
-            Name: name.value,
-            LastName: lastname.value,
-            Code: code.value,
-            FName:fName.value
-        }
-        nameList.push(reg);
-        nameListText += "<li> name: " + reg.Name + " lastname: " + reg.LastName + " code: " + reg.Code + " parent name: " + reg.FName + "</li>";
-        output.innerHTML = nameListText
-    } else {
-        alert('fill all the inputs')
+  if (!!name.value && !!lastname.value && !!code.value && !!fName.value) {
+    const reg = {
+      Name: name.value,
+      LastName: lastname.value,
+      Code: code.value,
+      FName: fName.value,
+    };
+    nameList.push(reg);
+    nameListText +=
+      "<li> name: " +
+      reg.Name +
+      " lastname: " +
+      reg.LastName +
+      " code: " +
+      reg.Code +
+      " parent name: " +
+      reg.FName +
+      "</li>";
+    output.innerHTML = nameListText;
+  } else {
+    console.log(111);
+    // alert('fill all the inputs');
+    if (
+      name.value == "" ||
+      lastname.value == "" ||
+      code.value == "" ||
+      fName.value == ""
+    ) {
+
+    if(name.value =="")
+      name.style = 'border:red 5px solid';
+    if (lastname.value == "") {
+      lastname.style = "border:red 5px solid";
+    }
+    if (code.value == "") {
+      code.style = "border:red 5px solid";
+    }
+    if (fName.value == "") {
+      fName.style = "border:red 5px solid";
+    }  
+     
     }
     
-
-})
+  }
+});
